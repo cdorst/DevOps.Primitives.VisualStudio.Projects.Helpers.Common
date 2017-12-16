fbd7d490e30f7efe042bbb896e3cddad79b0e14a
Christopher Dorst <chris@dorst.email>
2017-12-16 02:50:17 -0600

    Adding MsBuild PropertyLists and common properties helpers

---
+++ DevOps.Primitives.VisualStudio.Projects.Helpers.Common/PropertyLists.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DevOps.Primitives.VisualStudio.Projects.Helpers.Common
+{
+    public static class PropertyLists
+    {
+        public static MsBuildPropertyList Create(params MsBuildProperty[] properties)
+            => new MsBuildPropertyList(GetListItems(properties));
+
+        private static List<MsBuildPropertyListAssociation> GetListItems(params MsBuildProperty[] properties)
+            => properties.Select(property => new MsBuildPropertyListAssociation(property)).ToList();
+    }
+}
